b99ed80bc61c21b2e7aadcf15452e8ab5c3d5e20
Zander <akma_2003@mail.ru>
2021-02-05 19:40:21 +0300

    Выполнение ТЗ усложненного урока 7: 1) Создать массив week и записать в него дни недели в виде строк
    ·        Вывести на экран все дни недели
    ·        Каждый из них с новой строчки
    ·        Выходные дни - курсивом
    ·        Текущий день - жирным шрифтом(использовать объект даты)

--- lesson07/script.js
+++ lesson07/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+/*
+Создать массив week и записать в него дни недели в виде строк
+
+·        Вывести на экран все дни недели
+·        Каждый из них с новой строчки
+·        Выходные дни - курсивом
+·        Текущий день - жирным шрифтом(использовать объект даты)
+*/
+
+const week = ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+
+week.forEach(function(item, i) {
+    const br = document.createElement('br');
+    const today = new Date();
+    const weekDayToday = today.getDay();
+    console.log(weekDayToday);
+    if (i > 4) {
+        item = item.italics();
+    }
+    if (i === weekDayToday - 1) {
+        item = item.bold();
+    }
+    document.body.insertAdjacentHTML('beforeend', item);
+    document.body.append(br);
+});
