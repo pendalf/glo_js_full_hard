5e31dbb3def019f5804a10c348c864b349882724
Zander <akma_2003@mail.ru>
2021-02-06 18:24:55 +0300

    Задача
    Написать функцию генератор нечётных случайных чисел в диапазоне между x и y включительно.
    generateOddNumber(x, y)
    
    Учесть, что x и y могут быть отрицательными, а также может быть x > y или x < y. Вывести результат с помощью console.log.
    
    для проверки добавьте вызовы
    generateOddNumber(1, 100);
    generateOddNumber(0, -10);
    generateOddNumber(-7, -3);
    generateOddNumber(-100, 100);
    generateOddNumber(1, -1);

--- generateOddNumber/script.js
+++ generateOddNumber/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Функция для получения случайного чилса. Максимум и минимум включаются
+const generateOddNumber = function(min, max) {
+    const minNew = min < max ? Math.ceil(min) : Math.ceil(max);
+    const maxNew = min < max ? Math.floor(max) : Math.floor(min);
+
+    let random = Math.floor(Math.random() * (maxNew - minNew + 1)) + minNew;
+    if (random === 0 || !(random % 2)) {
+        random = generateOddNumber(min, max);
+    }
+    return random;
+};
+
+console.log(generateOddNumber(1, 100));
+console.log(generateOddNumber(0, -10));
+console.log(generateOddNumber(-8, -3));
+console.log(generateOddNumber(-100, 100));
+console.log(generateOddNumber(1, -1));
