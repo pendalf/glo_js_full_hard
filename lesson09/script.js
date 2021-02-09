3c4febb5fb0379c4046e02ff6b29f75b438c1b77
Zander <akma_2003@mail.ru>
2021-02-09 11:30:59 +0300

    Выполнение ТЗ усложненного урока 9:
    1) Выведите на страницу текущую дату и время в 2-х форматах:
    
        a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
        б) '04.02.2020 - 21:05:33'
    
    2) Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
    
    3) Для вывода в формате (б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры (из 9:5:3  1.6.2019 сделает 09:05:03 01.06.2019)
    
    4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду

--- lesson09/script.js
+++ lesson09/script.js
@@ -1,1 +1,74 @@
 'use strict';
+
+// Функция для добавления первого нуля к числу, если число однозначное
+const addZerroBefore = num => {
+    return num < 10 ? '0' + num : num + '';
+};
+
+// Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
+/* 
+ * Функция склонения элементов времени принимает переменные
+ * digit - цифра
+ * oneName - строка: час
+ * secondName - строка: часа
+ * thirdName - строка: часов
+ */
+const timeDeclension = (digit, oneName, secondName, thirdName) => {
+
+    let lastDigit = +digit.toString().slice(-1);
+    let output = `${digit} `;
+
+    if (
+        (lastDigit > 4 && lastDigit < 10) ||
+        lastDigit === 0 ||
+        (digit > 10 && digit < 15)
+
+    ) {
+        output += thirdName;
+    } else if (lastDigit > 1 && lastDigit < 5) {
+        output += secondName;
+    } else {
+        output += oneName;
+    }
+
+    return output;
+};
+
+const timeFormatString = (h, m, s) => {
+    let output = '';
+    output += timeDeclension(h, 'час', 'часа', 'часов');
+    output += ' ' + timeDeclension(m, 'минута', 'минуты', 'минут');
+    output += ' ' + timeDeclension(s, 'секунда', 'секунды', 'секунд');
+
+    return output;
+}
+
+const start = () => {
+    const today = new Date();
+    const fullDate = document.querySelector('.full-date');
+    const shortDate = document.querySelector('.short-date');
+    const timeUpdate = today => {
+        const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Ноября', 'Декабря', ]
+        const YY = today.getFullYear();
+        const MM = today.getMonth();
+        const DD = today.getDate();
+        const HH = today.getHours();
+        const ii = today.getMinutes();
+        const ss = today.getSeconds();
+        // const ms = today.getMilliseconds();
+        const day = days[today.getDay()];
+        const monthLong = months[MM - 1].toLowerCase();
+        // const day = 
+        fullDate.innerHTML = `Сегодня ${day}, ${DD} ${monthLong} ${YY} года, ${timeFormatString(HH, ii, ss)}`;
+        shortDate.innerHTML = `${addZerroBefore(DD)}.${addZerroBefore(MM)}.${YY} - ${addZerroBefore(HH)}:${addZerroBefore(ii)}:${addZerroBefore(ss)}`;
+    };
+    timeUpdate(today);
+    setTimeout(() => {
+        setInterval(() => {
+            timeUpdate(new Date());
+        }, 1000);
+    }, 1000 - today.getMilliseconds());
+};
+
+start();
