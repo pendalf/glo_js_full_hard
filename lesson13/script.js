c00e30e94ab3ad4ea037a145aa50c377e23c6bad
Zander <akma_2003@mail.ru>
2021-02-15 10:46:43 +0300

    Выполнение усложненного урока №13:
    - добавлен генератор случайного цвета
    - применение случайного цвета к фону страницы и изменение текста заголовка h1

--- lesson13/script.js
+++ lesson13/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const colorH1 = document.querySelector('#color'),
+    change = document.querySelector('#change');
+
+const colorRandom = function() {
+    const r = Math.floor(Math.random() * 256).toString(16),
+        g = Math.floor(Math.random() * 256).toString(16),
+        b = Math.floor(Math.random() * 256).toString(16);
+
+    return `#${r}${g}${b}`;
+};
+
+const render = function() {
+    const color = colorRandom();
+    document.body.style.background = color;
+    change.style.color = color;
+    colorH1.textContent = color;
+};
+
+change.addEventListener('click', render);
+
+render();
