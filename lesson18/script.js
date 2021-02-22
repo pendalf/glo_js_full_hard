c304e61e098d2fc31ae8a0cff84792323a94b910
Zander <akma_2003@mail.ru>
2021-02-22 12:54:56 +0300

    Выполнение усложненного урока №18 по ТЗ:
    Создайте отдельную HTML страницу с полем ввода <input> и пустым параграфом<p></p>
    
    Стилизация на ваше усмотрение
    Введённый в поле текст должен отображаться внутри параграфа, но с задержкой в 300 мс.
    
    При этом каждый введённый пользователем в поле символ сбрасывает предыдущий отложенный вызов и запускает новый.
    
    Таким образом программа должна ожидать завершения ввода пользователя и только после этого изменять текст в <p></p>

--- lesson18/script.js
+++ lesson18/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const _textInsert = function() {
+    const input = document.querySelector('#input'),
+        text = document.querySelector('#text');
+    return function() {
+        text.textContent = input.value;
+    }
+}
+
+const debounce = function(f, t) {
+    return function(args) {
+        let previousCall = this.lastCall || undefined;
+        this.lastCall = Date.now();
+        if (previousCall && ((this.lastCall - previousCall) <= t)) {
+            clearTimeout(this.lastCallTimer);
+        }
+        this.lastCallTimer = setTimeout(() => f(args), t);
+    };
+};
+
+const textInsert = _textInsert();
+
+input.addEventListener('input', debounce(textInsert, 300));
