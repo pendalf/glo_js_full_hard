f3b1e5b0f45b35e6e1789174a1322658c9a3ba9b
Zander <akma_2003@mail.ru>
2021-02-22 14:27:11 +0300

    Выполнение усложненного урока №18 по ТЗ:
    2) На отдельной HTML странице!!!
    
    Написать любую анимацию, используя requestAnimationFrame и кнопку, активирующую её
    
    Кнопка должна ставить анимацию на паузу и продолжать анимацию после повторного нажатия + Добавить кнопку reset, которая будет возвращать анимацию в первоначальное состояние

--- lesson18_2/script.js
+++ lesson18_2/script.js
@@ -1,1 +1,50 @@
 'use strict';
+const elem = document.querySelector('#animation'),
+    start = document.querySelector('#start'),
+    reset = document.querySelector('#reset'),
+    endPos = 100;
+
+let direction = 1,
+    play = 0,
+    count = 0,
+    animation;
+
+
+function render() {
+
+    if (count === endPos) {
+        direction = -1;
+    } else if (count === 0) {
+        direction = 1;
+    }
+
+    count += direction;
+
+    elem.style.left = count + 'px';
+
+}
+
+
+const animationLoop = function(e) {
+    render();
+    if (e.target && play) {
+        cancelAnimationFrame(animation);
+        play = 0;
+    } else {
+        animation = requestAnimationFrame(animationLoop, elem);
+        play = 1;
+    }
+};
+
+reset.onclick = function(e) {
+    count = -1;
+    direction = 1;
+    play = 0;
+    cancelAnimationFrame(animation);
+    render();
+};
+
+
+start.onclick = function(e) {
+    animationLoop(e);
+};
