07d1dcb365c16525ecc067097d77d456fd38abd4
Zander <akma_2003@mail.ru>
2021-03-08 23:46:26 +0300

    Выполнение усложненного задания урока №28 по ТЗ:
    1) Скачать файл ajax_car.zip
    2) Реализовать получение данных с помощью промисов
    3) Обязательно обработать ошибки

--- lesson28/script.js
+++ lesson28/script.js
@@ -1,28 +1,41 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
 
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
+    const getCarData = url => new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
+        request.open('GET', url);
         request.setRequestHeader('Content-type', 'application/json');
         request.send();
         request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
+
+            if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 200) {
                 const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
+                resolve(data);
             } else {
-                output.innerHTML = 'Произошла ошибка';
+                reject('Произошла ошибка');
             }
         });
+    });
+    const carView = data => {
+        data.cars.forEach(item => {
+            if (item.brand === select.value) {
+                const { brand, model, price } = item;
+                output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+            }
+        });
+    };
+
+    select.addEventListener('change', () => {
+        getCarData('./cars.json')
+            .then(carView)
+            .catch(err => output.innerHTML = err);
     });
 
 });
