03c541b727f6318729344ffb93c6cc9143bac1c2
Zander <akma_2003@mail.ru>
2021-03-15 23:43:45 +0300

    Установка @tailwindcss/custom-forms, добавление эффекта скрытия/раскрытия блока с чекбоксами, добавлена кнопка удаления фразы из поля поиска

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -1,4 +1,10 @@
 class Cards {
+    /**
+     * TODO:
+     * [ ] - исправить работу поиска при отключении всех фильтров
+     * [ ] - Исправить работу фильтра по фильмам при переключении на "--Choose film--"
+     */
+
     constructor(selector) {
         this.error = false;
         if (!selector) {
@@ -15,8 +21,10 @@ class Cards {
             this.search = new Set();
             this.paddingImg = 125;
             this.heroesList = document.createElement('div');
-            this.heroesList.className = "grid grid-cols-4 gap-4";
+            this.heroesList.className = "grid grid-cols-5 gap-5";
             this.filters = document.createElement('div');
+            this.filters.className = 'heroes__filters flex justify-between';
+            this.id = Cards.count;
         }
     }
 
@@ -31,6 +39,12 @@ class Cards {
         dataListener(val) {},
         registerNewListener(externalListenerFunction) { this.dataListener = externalListenerFunction; }
     }
+    // счетчик экземпляров класса
+    static get count() {
+        Cards._counter = (Cards._counter || 0) + 1;
+        return Cards._counter;
+    }
+
 
     // отслеживаем запуск fetch
     static fetching = false
@@ -76,15 +90,19 @@ class Cards {
         const select = document.createElement('select'),
             {
                 items,
-                classes,
                 attributes
             } = settings;
+
+        let  { classes } = settings;
+
 
         if (attributes) {
             Object.entries(attributes).forEach(([k, v]) => select.setAttribute(k, v));
         }
         if (classes) {
-            select.classList.add(classes);
+            classes = classes.split(' ');
+            classes.forEach(i => select.classList.add(i));
+
         }
         if (items?.length) {
             items.forEach(i => select.insertAdjacentHTML('beforeend', `<option value="${i}">${i}</option>`));
@@ -97,32 +115,43 @@ class Cards {
 
         const { label, checked } = name instanceof Object ? name : { checked: false, label: name };
         const checkbox = `<div class="heroes__form-item">
-                <label><input class="heroes__checkbox" type="checkbox" value="${label}"${checked ? ' checked' : ''}>${this.camelToNorm(label)}</label>
+                <label class="flex items-center mr-2 cursor-pointer">
+                    <input class="heroes__checkbox form-checkbox" type="checkbox" value="${label}"${checked ? ' checked' : ''}>
+                    <span class="ml-2">${this.camelToNorm(label)}</span>
+                </label>
             </div>`;
         return checkbox;
     }
 
     renderFilterMovies() {
         const items = [...this.movies].sort().reverse();
+        const selectWraper = document.createElement('div');
         items.push('--Choose film--');
         items.reverse();
 
         const select = this.renderSelect({
-            classes: 'filter-movie',
+            classes: 'filter-movie form-select h-14 pr-5 pl-5 rounded z-0 focus:shadow focus:outline-none',
             items,
             attributes: {
-                name: 'filterMovie',
+                name: `filter-movie-${this.id}`,
+                id: `filter-movie-${this.id}`
             }
         });
-        return select;
+        selectWraper.className = 'heroes__filter-movie-wrapper';
+        selectWraper.innerHTML = `<label for="filter-movie-${this.id}" class="mr-4">Filter by movie</label>`;
+        selectWraper.append(select);
+        return selectWraper;
     }
 
     renderSearch() {
-        let search = `<div class="heroes__search">`;
-        search += `<div class="relative"> <input type="text" class="heroes__search-input h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Search anything...">`;
-        search += `<div class="absolute top-4 right-3"> <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i> </div>`;
+        let search = `<div class="heroes__search flex-grow max-w-3xl mr-5">`;
+        search += `<div class="relative">`;
+        search += `<input type="text" class="heroes__search-input h-14 pr-16 pl-5 rounded w-full z-0 focus:shadow focus:outline-none" placeholder="Search anything...">`;
+        search += `<div class="absolute top-4 right-3"> <i class="fa fa-search text-gray-400 z-20 "></i> </div>`;
+        search += `<div class="heroes__search-close absolute top-4 right-10 hidden cursor-pointer"> <i class="fa fa-times-circle text-gray-400 z-20 hover:text-gray-500"></i> </div>`;
         search += `</div>`;
-        search += `<div class="heroes__search-extra">`;
+        search += `<button class="heroes__search-advanced focus:outline-none cursor-pointer hover:underline mt-2 mb-2">Advanced search</button>`;
+        search += `<div class="heroes__search-extra grid grid-cols-4 gap-3 overflow-hidden transition-all max-h-0 duration-700">`;
         [...this.fields].forEach(i => {
             if (i === 'name' || i === 'realName') {
                 this.search.add(i);
@@ -212,6 +241,7 @@ class Cards {
 
     searchHandler(e) {
         const target = e.target,
+            close = this.filters.querySelector('.heroes__search-close'),
             phrase = target.value,
             regexp = new RegExp(`(${phrase})`, 'gi'),
             search = [...this.heroes].map(i => ({ ...i })).filter(i => [...this.search].filter(s => {
@@ -220,8 +250,27 @@ class Cards {
                     return true;
                 }
             }).length);
+        if (phrase) {
+            close.classList.remove('hidden');
+        } else {
+            close.classList.add('hidden');
+        }
         this.renderCards(search);
 
+    }
+
+    clickHandler(e) {
+        const target = e.target;
+        if (target.closest('.heroes__search-close')) {
+            const input = this.filters.querySelector('.heroes__search-input');
+            input.value = '';
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+        }
+        if (target.closest('.heroes__search-advanced')) {
+            e.preventDefault();
+            const advanced = this.filters.querySelector('.heroes__search-extra');
+            advanced.classList.toggle('max-h-screen');
+        }
     }
 
     render() {
@@ -249,6 +298,7 @@ class Cards {
     handlers() {
         this.cardsContainer.addEventListener('change', this.changeHandler.bind(this));
         this.cardsContainer.addEventListener('input', this.searchHandler.bind(this));
+        this.cardsContainer.addEventListener('click', this.clickHandler.bind(this));
     }
 
 }
