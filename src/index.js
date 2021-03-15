e525c3ea0a516afa84685509e6561217d543bd7b
Zander <akma_2003@mail.ru>
2021-03-15 20:33:57 +0300

    Подключен шрифт awesome 5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 import watcher from './modules/watcher';
 import Cards from './modules/cards';
